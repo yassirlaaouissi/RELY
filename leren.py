e65e54ede5bd5dcb1c76e038580b75f8e7d7e0ff
romycatharinax <romycatharina@gmail.com>
2020-01-06 10:21:28 +0100

    Kan nu netjes worden bekeken, moet alleen nog naar een file worden geschreven.

--- leren.py
+++ leren.py
@@ -2,48 +2,60 @@ import psutil
 from datetime import datetime
 
 # De lijst die alle proccessen bevat in een lijst van dictionaries.
-processes = []
-for process in psutil.process_iter():
+def process_list():
+    processes = []
+    for process in psutil.process_iter():
     # Verkrijg alle proces informatie in een keer.
-    with process.oneshot():
-        # Verkrijg het proces ID
-        pid = process.pid
-        # krijg de naam van het uitgevoerde bestand
-        name = process.name()
-        # krijg de tijd dat het proces is voortgebracht
-        create_time = datetime.fromtimestamp(process.create_time())
-        try:
-            # verkrijg het aantal CPU-cores dat dit proces kan uitvoeren
-            cores = len(process.cpu_affinity())
-        except psutil.AccessDenied:
-            cores = 0
-            # verkrijg het CPU-gebruikpercentage
-        cpu_usage = process.cpu_percent()
-        # de status van het proces ophalen (actief, inactief, enz.)
-        status = process.status()
-        #try:
-            # get the process priority (a lower value means a more prioritized process)
-            #nice = int(process.nice())
-        #except psutil.AccessDenied:
-            #nice = 0
-        #try:
-            # get the memory usage of this process in bytes
-            #memory_usage = process.memory_full_info().uss
-        #except psutil.AccessDenied:
-            #memory_usage = 0
-        # totale proces gelezen en geschreven bytes
-        io_counters = process.io_counters()
-        read_bytes = io_counters.read_bytes
-        write_bytes = io_counters.write_bytes
-        # haal het aantal totale threads op dat door dit proces wordt voortgebracht
-        #n_threads = process.num_threads()
-        # ontvang de gebruikersnaam van gebruiker die het proces heeft voortgebracht
-        try:
-            username = process.username()
-        except psutil.AccessDenied:
-            username = "N/A"
+        with process.oneshot():
+            # Verkrijg het proces ID
+            pid = process.pid
+            # krijg de naam van het uitgevoerde bestand
+            name = process.name()
+            # krijg de tijd dat het proces is voortgebracht
+            create_time = datetime.fromtimestamp(process.create_time())
+            try:
+                # verkrijg het aantal CPU-cores dat dit proces kan uitvoeren
+                cores = len(process.cpu_affinity())
+            except psutil.AccessDenied:
+                cores = 0
+                # verkrijg het CPU-gebruikpercentage
+            cpu_usage = process.cpu_percent()
+            # de status van het proces ophalen (actief, inactief, enz.)
+            status = process.status()
+            #try:
+                # get the process priority (a lower value means a more prioritized process)
+                #nice = int(process.nice())
+            #except psutil.AccessDenied:
+                #nice = 0
+            try:
+                # get the memory usage of this process in bytes
+                memory_usage = process.memory_full_info().uss
+            except psutil.AccessDenied:
+                memory_usage = 0
+            # totale proces gelezen en geschreven bytes
+            io_counters = process.io_counters()
+            read_bytes = io_counters.read_bytes
+            write_bytes = io_counters.write_bytes
+            # haal het aantal totale threads op dat door dit proces wordt voortgebracht
+            #n_threads = process.num_threads()
+            # ontvang de gebruikersnaam van gebruiker die het proces heeft voortgebracht
+            try:
+                username = process.username()
+            except psutil.AccessDenied:
+                username = "N/A"
+    processes.append({
+        'pid' : pid, 'name' : name, 'create_time' : create_time, 'cores' : cores, 'cpu_usage' : cpu_usage, 'status' : status, 'memory_usage' : memory_usage, 'read_bytes' : read_bytes, 'write_bytes' : write_bytes, 'username' : username,
+    })
 
-processes.append({
-    'pid' : pid, 'name' : name, 'create_time' : create_time, 'cores' : cores, 'cpu_usage' : cpu_usage, 'status' : status, 'read_bytes' : read_bytes, 'write_bytes' : write_bytes, 'username' : username,
-})
+#def save_processlist_to_file(list_to_save):
+    #filename = 'processlist.txt'
+    #with open(filename, 'w') as f:
+        #f.write(" Process ID    ||  Name    ||  Create time     ||      Cores    ||     CPU usage       ||      Status      ||      Read bytes      ||      Write bytes     ||      Username    \n")
+        #want_to_print_list = input("Wil je de resultaten van het proces opslaan? (Y of N): ")
+        #for task in list_to_save:
+
+
+#if __name__ == '__main__':
+ #   process_list()
+
 
--- proces_list.py
+++ proces_list.py
@@ -1,56 +1,40 @@
 #Functionaliteit van Romy.
 import psutil
 from datetime import datetime
-import pandas as pd
+import tabulate
 
-
-def get_size(bytes):
-    """
-    Returns size of bytes in a nice format
-    """
-    for unit in ['', 'K', 'M', 'G', 'T', 'P']:
-        if bytes < 1024:
-            return f"{bytes:.2f}{unit}B"
-        bytes /= 1024
-
-
-# the list the contain all process dictionaries
+# Lijst die alle proces dictionaries bevat.
 processes = []
 for process in psutil.process_iter():
-    # get all process info in one shot
+    # Verkrijg alle proces informatie met one shot.
     with process.oneshot():
-        # get the process id
+        # Verkrijgen van het proces ID.
         pid = process.pid
-        # get the name of the file executed
+        # Naam verkrijgen van het uitgevoerde bestand.
         name = process.name()
-        # get the time the process was spawned
+        # Tijd verkrijgen van wanneer het proces is gecreeerd.
         create_time = datetime.fromtimestamp(process.create_time())
         try:
-            # get the number of CPU cores that can execute this process
+            # Verkrijg het aantal CPU-cores dat dit proces kan uitvoeren.
             cores = len(process.cpu_affinity())
         except psutil.AccessDenied:
             cores = 0
-        # get the CPU usage percentage
+        # Verkrijg het CPU-gebruikpercentage
         cpu_usage = process.cpu_percent()
-        # get the status of the process (running, idle, etc.)
+        # Verkrijg de status van het proces.
         status = process.status()
         try:
-            # get the process priority (a lower value means a more prioritized process)
-            nice = int(process.nice())
-        except psutil.AccessDenied:
-            nice = 0
-        try:
-            # get the memory usage in bytes
+            # Verkrijgen van het geheugen gebruik in bytes.
             memory_usage = process.memory_full_info().uss
         except psutil.AccessDenied:
             memory_usage = 0
-        # total process read and written bytes
+        # Totale proces gelezen en geschreven bytes.
         io_counters = process.io_counters()
         read_bytes = io_counters.read_bytes
         write_bytes = io_counters.write_bytes
-        # get the number of total threads spawned by this process
+        # Het ophalen van het aantal totale threads dat door het proces wordt voortgebracht
         n_threads = process.num_threads()
-        # get the username of user spawned the process
+        # Ontvang de gebruikersnaam van gebruiker die het proces heeft voortgebracht
         try:
             username = process.username()
         except psutil.AccessDenied:
@@ -58,48 +42,11 @@ for process in psutil.process_iter():
 
     processes.append({
         'pid': pid, 'name': name, 'create_time': create_time,
-        'cores': cores, 'cpu_usage': cpu_usage, 'status': status, 'nice': nice,
+        'cores': cores, 'cpu_usage': cpu_usage, 'status': status,
         'memory_usage': memory_usage, 'read_bytes': read_bytes, 'write_bytes': write_bytes,
         'n_threads': n_threads, 'username': username,
     })
 
-# convert to pandas dataframe
-df = pd.DataFrame(processes)
-# set the process id as index of a process
-df.set_index('pid', inplace=True)
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Process Viewer & Monitor")
-    parser.add_argument("-c", "--columns", help="""Columns to show,
-                                                available are name,create_time,cores,cpu_usage,status,nice,memory_usage,read_bytes,write_bytes,n_threads,username.
-                                                Default is name,cpu_usage,memory_usage,read_bytes,write_bytes,status,create_time,nice,n_threads,cores.""",
-                        default="name,cpu_usage,memory_usage,read_bytes,write_bytes,status,create_time,nice,n_threads,cores")
-    parser.add_argument("-s", "--sort-by", dest="sort_by", help="Column to sort by, default is memory_usage .",
-                        default="memory_usage")
-    parser.add_argument("--descending", action="store_true", help="Whether to sort in descending order.")
-    parser.add_argument("-n", help="Number of processes to show, will show all if 0 is specified, default is 25 .",
-                        default=25)
-
-    # parse arguments
-    args = parser.parse_args()
-    columns = args.columns
-    sort_by = args.sort_by
-    descending = args.descending
-    n = int(args.n)
-
-    # sort rows by the column passed as argument
-    df.sort_values(sort_by, inplace=True, ascending=not descending)
-    # pretty printing bytes
-    df['memory_usage'] = df['memory_usage'].apply(get_size)
-    df['write_bytes'] = df['write_bytes'].apply(get_size)
-    df['read_bytes'] = df['read_bytes'].apply(get_size)
-    # convert to proper date format
-    df['create_time'] = df['create_time'].apply(datetime.strftime, args=("%Y-%m-%d %H:%M:%S",))
-    # reorder and define used columns
-    df = df[columns.split(",")]
-    # print
-    if n == 0:
-        print(df.to_string())
-    elif n > 0:
-        print(df.head(n).to_string())
+header = processes[0].keys()
+rows = [x.values() for x in processes]
+print(tabulate.tabulate(rows, header, tablefmt='rst'))
